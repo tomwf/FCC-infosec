1fde2a0c3dbcbebfeb0d8537261e18943747d4ee
tomwf <chowkirthmas@gmail.com>
2022-09-12 16:05:03 +0200

    add frameguard()

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy())
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard());
 
 
 
