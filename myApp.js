eb6f7b2aac4d3f87dcab4f20e90f9981d508fbc8
tomwf <chowkirthmas@gmail.com>
2022-09-12 19:00:28 +0200

    fix dnsPrefetchControl

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
-app.use(helmet.dnsPrefetchControl({ allow: true }))
+app.use(helmet.dnsPrefetchControl())
 
 
 
