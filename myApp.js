30fb322d42c40de03a7be1ebe5ed841282664a3d
tomwf <chowkirthmas@gmail.com>
2022-09-12 21:25:16 +0200

    add contentSecurityPolicy()

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-const ninetyDaysInSeconds = 90*24*60*60
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
@@ -11,7 +11,12 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    "default-src": ["'self'"],
+    "script-src": ["'self'", "trusted-cdn.com"]
+  }
+}));
 
 
 
@@ -60,7 +65,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
